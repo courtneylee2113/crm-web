c6a088f9393dd4cd225f51fb7e52084ecb8a65a7
Courtney Lee <courtneylee2113@gmail.com>
2015-04-08 16:15:01 -0400

    added second half of rolodex and new contact form

--- crm.rb
+++ crm.rb
@@ -6,7 +6,6 @@ get '/' do
   @crm_app_name = "My CRM"
   erb :index
 end
-
 get "/contacts" do
   @contacts = []
   @contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
@@ -14,4 +13,17 @@ get "/contacts" do
   @contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
 
   erb :contacts
+end
+
+get '/contacts/new' do
+  erb :new_contact
+end
+post '/contacts' do
+  puts params
+end
+
+post '/contacts' do
+  new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+  $rolodex.add_contact(new_contact)
+    redirect to('/contacts')
 end
